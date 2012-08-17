d37a30cb8a40fe6c2a552b010338c21e413814c7
Lee Farley <lee@lee-VirtualBox.(none)>
2012-08-17 17:18:46 +1200

    added in image sizes to paperclip

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,6 +18,7 @@ class Project < ActiveRecord::Base
 
   has_attached_file :thumbnail,:styles => {:thumb  => "100x100"},
   		:storage => :s3,
+  		:url => ":s3_domain_url",
         :s3_credentials => {:bucket => 'robynaadamsondesign',:access_key_id => 'AKIAIO4KDFY5CJEKTYBQ',:secret_access_key => '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'},
   		:path => ':attachment/:id/:style.:extension'
 end
