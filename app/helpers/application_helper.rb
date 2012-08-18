d1eee7ecb5858f97cb303bdf65f91db6afebd52d
Lee Farley <lee@lee-VirtualBox.(none)>
2012-08-19 10:12:14 +1200

    added in jquery to switch tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,7 @@ def nav_link(link_text, link_path)
     link_to link_text, link_path
   end
 end
+
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
