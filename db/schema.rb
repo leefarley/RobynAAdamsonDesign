214f59f4dd16d7f5d11ecdc9ce63f1d027c6534c
Lee Farley <lee@lee-VirtualBox.(none)>
2012-08-19 12:04:58 +1200

    adding migration with all projects on it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120817075352) do
+ActiveRecord::Schema.define(:version => 20120818233835) do
 
   create_table "images", :force => true do |t|
     t.integer  "project_id"
