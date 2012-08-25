c1e551b761165f6d73c811426e727824be84cabe
Lee Farley <lee@lee-VirtualBox.(none)>
2012-08-25 14:23:13 +1200

    added devise authentication to admin area

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email: 'robyn.a.adamson@gmail.com',password: 'F@cebook1sgay')
+User.create(email: 'Lee@robynaadamsondesign.com',password: 'password')
